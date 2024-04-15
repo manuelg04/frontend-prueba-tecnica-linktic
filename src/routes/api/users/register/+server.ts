ec5719fd742eca8d3aa0e5519e54b90b04aa0815
Manuel Gonzalez <manuelg9704@gmail.com>
2024-04-15 16:42:14 -0500

    Refactor registerUser function to return a Response object with appropriate headers

--- src/routes/api/users/register/+server.ts
+++ src/routes/api/users/register/+server.ts
@@ -1,6 +1,17 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import { registerUser } from '$lib/server/user.controller';
 
-export const POST = async (event: RequestEvent) => {
-  return await registerUser(event);
+export const POST = async (event: RequestEvent): Promise<Response> => {
+  const response = await registerUser(event);
+  
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  };
+
+  return new Response(JSON.stringify(response.body), {
+    status: response.status,
+    headers: headers,
+  });
 };
